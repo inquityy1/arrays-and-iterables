89d7c10cdc63a59fd67af4d1e5a0d342de8fd13c
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-04 18:41:01 +0100

    maps

---
+++ setsmaps.js
@@ -0,0 +1,52 @@
+/*const ids = new Set(['hi', 'from' , 'set']);
+ids.add(2);
+ids.delete('hi');
+
+for (const entry of ids.entries()) {
+	console.log(entry);
+}*/
+
+const person1 = {name: 'max'};
+const person2 = {name: 'manuel'};
+
+const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
+
+personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+for (const [key, value] of personData.entries()) {
+	console.log(key, value);
+}
+
+for (const value of personData.values()) {
+	console.log(value);
+}
+
+for (const keys of personData.keys()) {
+	console.log(keys);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
