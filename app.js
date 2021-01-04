8cd61addee12aad55dee84d82d5870928d293aa5
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-04 16:27:11 +0100

    map,sort,reverse,filter,reduce

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ const maxIndex = personData.findIndex((person, idx, persons) => {
 });
 
 console.log(maxIndex);
-*/
+
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 const taxAdjustedPrice = [];
@@ -86,8 +86,46 @@ prices.forEach((price, idx, prices) => {
 });
 
 console.log(taxAdjustedPrice);
+*/
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
 
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
+	const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)};
+	return priceObj;
+});
 
+// console.log(prices, taxAdjustedPrices);
+
+const sortedPrices = prices.sort((a, b) => {
+	if (a > b) {
+		return 1;
+	} else if (a === b) {
+		return 0;
+	} else {
+		return -1;
+	}
+});
+//console.log(sortedPrices.reverse());
+console.log(sortedPrices);
+
+const filteredArray = prices.filter(p => p > 6);
+
+console.log(filteredArray);
+
+//let sum = 0;
+
+//prices.forEach((price) => {
+//	sum += price
+//});
+
+//console.log(sum);
+
+const sum = prices.reduce((prevValue, curValue, curIndex, prices) => {
+	return prevValue + curValue;
+}, 0);
+
+console.log(sum);
 
 
 
